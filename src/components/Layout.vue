ea52246776967cadf2b15e1e56d5dbab1c2fb289
Janos Perge <janos.perge@gmail.com>
2024-03-07 23:01:44 +0100

    add primevue, start layout

---
+++ src/components/Layout.vue
@@ -0,0 +1,10 @@
+<script setup>
+import LeftSide from './LeftSide.vue'
+import RightSide from './RightSide.vue'
+</script>
+<template>
+  <div id="layout">
+    <LeftSide />
+    <RightSide />
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import './assets/main.css'
+import './assets/css/main.css'
+import 'primevue/resources/themes/aura-light-blue/theme.css'
+// import './assets/scss/theme.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue).mount('#app')
